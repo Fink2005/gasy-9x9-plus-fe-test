e1049070a8e226a0890a9c8988f13183353814c3
Fink2005 <phans3806@gmail.com>
2025-08-10 17:42:39 +0700

    chore: change admin address

--- src/libs/shared/constants/globals.ts
+++ src/libs/shared/constants/globals.ts
@@ -1,5 +1,5 @@
 export const COUNT_ITEM_TO_BOTTOM = 3;
 export const USER_RANKING_LIMIT = 10;
-export const ADMIN_SYSTEM_ADDRESS = '0xd01498c3178d75ee69271ea62ad13e8c523e8038';
+export const ADMIN_SYSTEM_ADDRESS = '0xc30a8e1ad70acd22c6350ba9d74e09f05574f672';
 export const BOX_TREE_LIMIT = 9;
 export const CACHE_REVALIDATE_MINUTES = 60 * 5; // 5 minutes
